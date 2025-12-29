cc03820b552049ce08d2cd7d5f2c2b4740ef7981
GG2802 <>
2025-12-29 20:56:56 +0000

    Final Variables

--- deep_java_structure/final_variables/Main.java
+++ deep_java_structure/final_variables/Main.java
@@ -3,17 +3,17 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         // initializing variables
-        int a = 9;
-        int b = 15;
+         int a = 9;
+        final int b = 15;
         int c = 21;
         int d = -62;
-        int e = 0;
+        final int e = 0;
         
         // performing mathematical operations on the variables
         c = c + 9;
         d = d - 8;
         a = a + 1;
-        int result = a + b + c + d + e;
+        final int result = a + b + c + d + e;
         
         // displaying the result
         System.out.println(result);
