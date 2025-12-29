dd38128cd303e7403570865bddc746dfe2271b2c
GG2802 <>
2025-12-29 21:21:38 +0000

    Ternary Operator

---
+++ deep_java_structure/ternary_operator/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String string = "California, Texas, New York, Florida, Alaska";
+        System.out.println(
+                        string.contains("Florida") 
+                         ? "The string contains Florida" 
+                         : "Florida not Not found" );
+    }
+}
