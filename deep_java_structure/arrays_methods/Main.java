a8dd3055a91990cdf963820dd59471c8c9511c49
GG2802 <>
2025-12-29 21:04:50 +0000

    Arrays methods

---
+++ deep_java_structure/arrays_methods/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+// import the Arrays library here
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] unsortedArray = {'y', 'e', 'e', 'p', 'w', 'c', '>', '3'};
+        // sort the array and output it to the console
+        Arrays.sort(unsortedArray);
+        System.out.println(unsortedArray);
+    }
+}
